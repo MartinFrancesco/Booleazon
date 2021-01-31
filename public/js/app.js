928f12105dca92025a6461a15833ee5f78393c61
Luis <s.ros.lui@gmail.com>
2021-01-31 11:19:24 +0100

    ROBE incasinate

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\David\Desktop\Booleazon-master\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\David\Desktop\Booleazon-master\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\MAMP\htdocs\Booleazon-master\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\MAMP\htdocs\Booleazon-master\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,8 @@
     {{-- Vista --}}
     <h1>HOME PROVA</h1>
 
-
-    <div>
+    {{-- CARD (DAVID--> VA IN SHOP) --}}
+    {{-- <div>
         <div class="container-card">
                     <div class="card" id="salsa">
                         <div class="img-bx">
@@ -23,5 +23,18 @@
                         </div>
                     </div>
             </div>
-    </div>
+    </div> --}}
+
+
+    <!-- The video -->
+    <video autoplay muted loop id=“myVideo”>
+    <source src=“rain.mp4" type=“video/mp4”>
+    </video>
+   <!-- Optional: some overlay text to describe the video -->
+    <div class=“content”>
+    <h1>Heading</h1>
+    <p>Lorem ipsum...</p>
+    <!-- Use a button to pause/play the video with JavaScript -->
+    <button id=“myBtn” onclick=“myFunction()“>Pause</button>
+   </div>
 @endsection
