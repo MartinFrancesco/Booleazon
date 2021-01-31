579d21ac65cbfe9a4f5c4292475b77e66374c8a5
Luis <s.ros.lui@gmail.com>
2021-01-31 10:55:27 +0100

    add show &markup

--- resources/views/cds/show.blade.php
+++ resources/views/cds/show.blade.php
@@ -1,5 +1,43 @@
 @extends("layouts.main")
 
 @section("content")
-   <h1>SHOW PAGE</h1>
+   <div class="container">
+      <h1 class="mb-5">
+         {{$cd->titolo}} details page
+     </h1>
+     <img width="200" src=" {{ $cd->cover }} " alt="{{ $cd->titolo }}">
+     <h2>
+         ID: {{$cd->id}}
+     </h2>
+     <h4>
+         Artista: {{$cd->artista}}
+     </h4>
+      <h4>
+         Genere: {{$cd->genere}}
+      </h4>
+      <h4>
+         Anno: {{$cd->anno}}
+      </h4>
+      <h4>
+         Prezzo: {{$cd->prezzo}}
+     </h4>
+     <h4>
+         Disponibilità:: {{$cd->disponibilità}}
+      </h4>
+     <hr>
+     {{-- update & created CD --}}
+     <h2>
+         Created at:
+      </h2>
+      <h4>
+          {{$cd->created_at->isoFormat('dddd DD/MM/YYYY')}}
+      </h4>
+      <hr>
+      <h2>
+         Updated at:
+      </h2>
+      <h4>
+         {{$cd->updated_at->diffForHumans()}}
+      </h4> 
+   </div>
 @endsection
